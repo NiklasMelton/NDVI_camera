40d0ed5fd1e7e80a5f4d494005ce3944a3cf99fe
unknown <niklasmelton@gmail.com>
2020-10-04 00:54:11 -0500

    main

--- ArduCamMulti.py
+++ ArduCamMulti.py
@@ -7,7 +7,7 @@ i2c_address = 0x70
 
 class MultiCamera:
     def __init__(self):
-        self.bus = smbus.SMBus(i2c_ch)
+        # self.bus = smbus.SMBus(i2c_ch)
         gpio.setmode(gpio.BCM)
         gpio.setup(17, gpio.OUT)
         gpio.setup(4, gpio.OUT)
@@ -16,11 +16,12 @@ class MultiCamera:
 
     def select_camera_A(self):
         gpio.output(4, 0)
-        self.bus.write_i2c_block_data(i2c_address, 0x00, 0x01)
-
+        # self.bus.write_i2c_block_data(i2c_address, 0x00, 0x01)
+        os.system('i2cset -y 1 0x70 0x00 0x01')
     def select_camera_B(self):
         gpio.output(4, 1)
-        self.bus.write_i2c_block_data(i2c_address, 0x00, 0x02)
+        # self.bus.write_i2c_block_data(i2c_address, 0x00, 0x02)
+        os.system('i2cset -y 1 0x70 0x00 0x02')
 
     def capture(self,name):
         cmd = "raspistill -o {}.jpg",format(name)
