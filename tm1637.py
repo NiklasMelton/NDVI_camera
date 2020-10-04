033a5e8854b57d343ab5bb68861e5b21cbda522c
unknown <niklasmelton@gmail.com>
2020-10-04 01:38:34 -0500

    showint

--- tm1637.py
+++ tm1637.py
@@ -8,7 +8,7 @@ from time import sleep, localtime
 IO.setmode(IO.BCM)
 
 HexDigits = [0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d,
-             0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x40]
+             0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71, 0x7f, 0x40]
 
 ADDR_AUTO = 0x40
 ADDR_FIXED = 0x44
@@ -49,9 +49,12 @@ class TM1637:
 
     def ShowInt(self, i):
         s = str(i)
+        ns = len(s)
         self.Clear()
-        for i in range(0, len(s)):
-            self.Show1(i, int(s[i]))
+        for i in range(0,4-ns):
+            self.Show1(i, -2)
+        for i in range(0, ns):
+            self.Show1(i+(4-ns), int(s[i]))
 
     def Show(self, data):
         for i in range(0, 4):
