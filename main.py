a42c40b7f4865fdbfa330fde625bfde4c65bdc2a
unknown <niklasmelton@gmail.com>
2020-10-05 00:30:10 -0500

    callable

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class camera_box:
         self.IMAGE_COUNT = initialize_image_counter()
 
         gpio.setup(BUTTON_CHANNEL, gpio.IN, pull_up_down=gpio.PUD_UP)
-        gpio.add_event_detect(BUTTON_CHANNEL, gpio.FALLING, callback=self.shutter,bouncetime=200)
+        gpio.add_event_detect(BUTTON_CHANNEL, gpio.FALLING, callback=self.callback_shutter,bouncetime=200)
 
     def shutter(self,today=None):
         filename = DATA_PATH+gen_filename(today)
@@ -81,6 +81,10 @@ class camera_box:
         self.display.show_int(self.IMAGE_COUNT)
         time.sleep(3)
         self.DMUTEX = False
+
+    def callback_shutter(self,channel):
+        print('Button pressed, channel '+channel)
+        self.shutter()
 
 
 if __name__ == '__main__':
